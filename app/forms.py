859e0d6d19466e25e51ab38e3943cfadef371171
TheSaviourEking <saviour.eking@outlook.com>
2024-09-16 16:20:41 +0100

    chore: implement Handling the form submission

---
+++ app/forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import (
+    StringField,
+    DateField,
+    TimeField,
+    TextAreaField,
+    BooleanField,
+    SubmitField,
+)
+from wtforms.validators import DataRequired
+
+
+class AppointmentForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    start_date = DateField("Start Date", validators=[DataRequired()])
+    start_time = TimeField("Start Time", validators=[DataRequired()])
+    end_date = DateField("End Date", validators=[DataRequired()])
+    end_time = TimeField("End Time", validators=[DataRequired()])
+    description = TextAreaField("Description", validators=[DataRequired()])
+    private = BooleanField("Private Event ?")
+    submit = SubmitField("Create an appointment")
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from os import environ
 import psycopg2
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, request, flash
+from .forms import AppointmentForm
+from datetime import datetime
 
 CONNECTION_PARAMETERS = {
     "user": environ.get("DB_USER"),
@@ -8,14 +10,55 @@ CONNECTION_PARAMETERS = {
     "dbname": environ.get("DB_NAME"),
     "host": environ.get("DB_HOST"),
 }
+
 bp = Blueprint("main", __name__, url_prefix="/")
 
 
-@bp.route("/")
+@bp.route("/", methods=["GET", "POST"])
 def main():
     """
     docstring to clear this error
     """
+    form = AppointmentForm()
+
+    if form.validate_on_submit():
+        print(
+            {
+                "name": form.name.data,
+                "start_datetime": datetime.combine(
+                    form.start_date.data, form.start_time.data
+                ),
+                "end_datetime": datetime.combine(
+                    form.end_date.data, form.end_time.data
+                ),
+                "description": form.description.data,
+                "private": form.private.data,
+            }
+        )
+
+        with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+            with conn.cursor() as curs:
+                curs.execute(
+                    """
+                        INSERT into appointments(
+                        name, start_datetime, end_datetime, description, private
+                        )
+                        VALUES (%(name)s, %(start_datetime)s, %(end_datetime)s, %(description)s, %(private)s)
+                    """,
+                    {
+                        "name": form.name.data,
+                        "start_datetime": datetime.combine(
+                            form.start_date.data, form.start_time.data
+                        ),
+                        "end_datetime": datetime.combine(
+                            form.end_date.data, form.end_time.data
+                        ),
+                        "description": form.description.data,
+                        "private": form.private.data,
+                    },
+                )
+                
+                return redirect("/")
 
     with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
         with conn.cursor() as curs:
@@ -27,4 +70,4 @@ def main():
             )
 
             rows = curs.fetchall()
-    return render_template("main.html", rows=rows)
+            return render_template("main.html", rows=rows, form=form)
